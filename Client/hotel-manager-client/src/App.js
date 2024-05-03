14c988a2e2c88be961b63e1ac5e900739f71d64c
PiravenNan <xothspath@gmail.com>
2024-05-03 19:54:53 +0100

    Added HotelManagerContainer

--- Client/hotel-manager-client/src/App.js
+++ Client/hotel-manager-client/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import HotelManagerContainer from './containers/HotelManagerContainer';
 
 function App() {
   return (
-
+    <HotelManagerContainer/>
   );
 }
 
---
+++ Client/hotel-manager-client/src/containers/HotelManagerContainer.js
@@ -0,0 +1,12 @@
+import { useEffect, useState, createContext } from "react";
+
+
+const HotelManagerContainer = () => {
+    return (
+        <>
+            <h2>Hello from HotelManagerContainer!</h2>
+        </>
+    );
+}
+
+export default HotelManagerContainer;
