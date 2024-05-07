7bf2bfa8ee37ce4598f84dee90d948fd8d0e64f2
PiravenNan <xothspath@gmail.com>
2024-05-07 19:21:45 +0100

    Update HotelManagerContainer.js

--- Client/hotel-manager-client/src/containers/HotelManagerContainer.js
+++ Client/hotel-manager-client/src/containers/HotelManagerContainer.js
@@ -4,7 +4,7 @@ import { useEffect, useState, createContext } from "react";
 const HotelManagerContainer = () => {
     return (
         <>
-            <h2>Hello From The HotelManagerContainer</h2>
+            <h2>Hello From the HotelManagerContainer</h2>
         </>
     );
 }
