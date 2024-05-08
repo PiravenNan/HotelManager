10170067bcf5c56aa353ebd2fc9a0515c3a37e09
PiravenNan <xothspath@gmail.com>
2024-05-08 22:24:49 +0100

    Added LogInForm

---
+++ Client/hotel-manager-client/src/components/LogInForm.js
@@ -0,0 +1,5 @@
+const LogInForm = () =>{
+    return (<h2>Hi! :D</h2>);
+}
+
+export default LogInForm;
--- Client/hotel-manager-client/src/containers/HotelManagerContainer.js
+++ Client/hotel-manager-client/src/containers/HotelManagerContainer.js
@@ -1,10 +1,11 @@
 import { useEffect, useState, createContext } from "react";
+import LogInForm from "../components/LogInForm";
 
 
 const HotelManagerContainer = () => {
     return (
         <>
-            <h2>Hello From the HotelManagerContainer</h2>
+            <LogInForm/>
         </>
     );
 }
